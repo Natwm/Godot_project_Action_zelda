602d5d06695db97431925692bab410e478f859a0
NatWazze <mezza@blackfoot.io>
2024-07-29 16:24:11 +0200

    NMezza - [Feature] IA State V2

---
+++ Scripts/StatePattern/Base_adversaire/Base_adversaire_IDLE.gd
@@ -0,0 +1,22 @@
+extends State
+
+
+
+func update(_delta: float) -> void:
+	pass
+
+
+# Virtual function. Called by the state machine upon changing the active state. The `msg` parameter
+# is a dictionary with arbitrary data the state can use to initialize itself.
+func enter(_msg := {}) -> void:
+	owner.player_detection_area.monitorable = true
+	owner.player_detection_area.monitoring = true
+	pass
+
+# Virtual function. Called by the state machine before changing the active state. Use this function
+# to clean up the state.
+func exit() -> void:
+	owner.player_detection_area.monitorable = false
+	owner.player_detection_area.monitoring = false
+	pass
+
